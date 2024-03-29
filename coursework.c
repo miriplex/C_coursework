72b9fd45c464ab5a6d698c469f56e902f2c61904
Alex Casson <alex.casson@manchester.ac.uk>
2024-03-29 20:31:07 +0000

    Added coursework files

---
+++ coursework.c
@@ -0,0 +1,28 @@
+// Add brief description here
+
+#include <stdio.h>
+// Enter any other #includes that you deem necessary below
+
+
+#define NUMROWS	 9    // map size
+#define NUMCOLS  9    // map size
+#define NUMWALKS 1000 // number of random walks
+// Enter any other #defines as you deem necessary below
+
+
+int main(void) {
+
+    // Load the map
+
+
+
+    // Perform random walks and calculate results
+
+
+
+    // Print results
+    
+
+
+    return 0;
+}
